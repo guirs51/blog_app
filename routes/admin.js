93a7d7d30b7092558713449b65f5ebeb7dfe333f
Guilherme <guilhermediasvillanova@gmail.com>
2025-06-20 15:31:42 -0300

    feat: (atualizar e deletar): adicionado as feature de atualizar e deletar categorias

--- routes/admin.js
+++ routes/admin.js
@@ -9,13 +9,9 @@ router.get('/', (req, res) => {
     res.render('admin/index');
 })
 
-router.get('/post', (req, res) => {
-    res.send("Pagina de post")
-})
-
 router.get('/categorias', (req, res) => {
-    Categoria.find().sort({data: "desc"}).then((categorias) => {
-        res.render("admin/categorias", { 
+    Categoria.find().sort({ data: "desc" }).then((categorias) => {
+        res.render("admin/categorias", {
             categorias: categorias,
             success_msg: req.flash('success_msg'),
             err_msg: req.flash('err_msg')
@@ -64,17 +60,59 @@ router.post('/categorias/nova', (req, res) => {
 });
 
 router.get('/categorias/edit/:id', (req, res) => {
-    Categoria.findOne({_id: req.params.id}).then((categorias) => {
-        res.render('admin/editcategoria' , {categorias: categorias});
+    Categoria.findOne({ _id: req.params.id }).then((categorias) => {
+        res.render('admin/editcategoria', { categorias: categorias });
+    }).catch((err) => {
+        console.log(err);
     })
-    //  res.render('admin/editcategoria');
-     
 });
 
-// router.get('/test-flash', (req, res) => {
-//     req.flash('success_msg', 'Mensagem de teste - sucesso!');
-//     req.flash('err_msg', 'Mensagem de teste - erro!');
-//     res.redirect('/admin/categorias');
-// });
+router.post('/categorias/edit', (req, res) => {
+    let erros = [];
+
+    if (!req.body.nome || typeof req.body.nome === undefined || req.body.nome === null) {
+        erros.push({ texto: "nome não pode ser vazio" });
+    }
+
+    if (!req.body.slug || typeof req.body.slug === undefined || req.body.slug === null) {
+        erros.push({ texto: "Slug inválido" });
+    }
+
+    if (erros.length > 0) {
+        res.render('admin/editcategoria', { erros: erros });
+    } else {
+        Categoria.findOne({ _id: req.body.id }).then((categoria) => {
+            categoria.nome = req.body.nome;
+            categoria.slug = req.body.slug;
+
+            categoria.save().then(() => {
+                req.flash("success_msg", "Categoria editada com sucesso");
+                res.redirect('/admin/categorias');
+            }).catch((err) => {
+                req.flash("err_msg", "Houve um erro ao editar a categoria");
+                res.redirect('/admin/categorias');
+            });
+        }).catch((err) => {
+            req.flash("err_msg", "Houve um erro ao encontrar a categoria");
+            res.redirect('/admin/categorias');
+        });
+    }
+});
+
+router.post('/categorias/deletar', (req, res) => {
+    Categoria.deleteOne({ _id: req.body.id }).then(() => {
+        req.flash("success_msg", "Categoria deletada com sucesso!");
+        res.redirect("/admin/categorias");
+    }).catch((err) => {
+        req.flash("err_msg", "Erro ao deletar categoria!!");
+        res.redirect("/admin/categorias")
+    })
+})
+
+router.get('/test-flash', (req, res) => {
+    req.flash('success_msg', 'Mensagem de teste - sucesso!');
+    req.flash('err_msg', 'Mensagem de teste - erro!');
+    res.redirect('/admin/categorias');
+});
 
 module.exports = router
